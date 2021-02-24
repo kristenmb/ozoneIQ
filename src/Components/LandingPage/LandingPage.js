022a192dfe38f9429f28fc8b23cc2222df8b56e4
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-24 12:22:42 -0700

    Set up basic layout and styling for landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,25 +1,32 @@
 import React, { useState } from 'react'
 import './LandingPage.css'
 import searchIcon from '../../assets/search.png'
+import sun from '../../assets/sun.svg'
 
 function LandingPage() {
   const [currentLocation, setCurrentLocation] = useState('')
   const [chooseLocation, setChooseLocation] = useState('')
 
   return (
-    <form>
-      <button className='currentLocal'>Get Current Location</button>
-      <section className='chooseLocal'>
-        <input 
-          type='text'
-          placeholder='City, State'
-          name='chooseLocation'
-          value={chooseLocation}
-          onChange={event => setChooseLocation(event.target.value)}
-        />
-        <img className='searchButton' src={searchIcon} />
-      </section>
-    </form>
+    <section className='landingPage'>
+      <h1 className='title'>OzoneIQ</h1>
+      <div className='sunWrapper'>
+        <img className='sun' src={sun} />
+      </div>
+      <form>
+        <button className='currentLocal'>Get Current Location</button>
+        <section className='chooseLocal'>
+          <input 
+            type='text'
+            placeholder='City, State'
+            name='chooseLocation'
+            value={chooseLocation}
+            onChange={event => setChooseLocation(event.target.value)}
+          />
+          <img className='searchButton' src={searchIcon} />
+        </section>
+      </form>
+    </section>
     
   )
 }
