775516daabbfd35ba5746dbfd26ac33a00cf7aff
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-24 10:50:05 -0700

    Build landing page component and import into app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import './App.css';
+import LandingPage from '../LandingPage/LandingPage'
 
 function App() {
   return (
     <div className="App">
       <h1>APP</h1>
+      <LandingPage />
     </div>
   );
 }
---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './LandingPage.css'
+
+function LandingPage() {
+
+
+  return (
+    <h1>LandingPage</h1>
+  )
+}
+
+export default LandingPage
