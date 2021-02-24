2f5abb6e8ab139ec97ed14ac84d9023640baf96a
Kristen Bair <kristenmbair@gmail.com>
2021-02-24 11:09:48 -0700

    Create initial build of AqiInfo component with imports and exports

---
+++ src/Components/AqiInfo/AqiInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './AqiInfo.css'
+
+const AqiInfo = () => {
+  return (
+    <section className="aqi-section">
+      <h2>hi i'm the aqi info</h2>
+
+    </section>
+  )
+}
+
+export default AqiInfo
