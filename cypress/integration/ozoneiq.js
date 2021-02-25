36d0fa2c37abb1dab78f0a17ba59fa86bf4b6bdd
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-25 13:13:21 -0700

    Add testing for dashboard

--- cypress/integration/ozoneiq.js
+++ cypress/integration/ozoneiq.js
@@ -1,12 +1,5 @@
-describe('OzoneIQ', () => {
+describe('OzoneIQ Landing Page', () => {
   beforeEach(() => {
-    cy.fixture('aqiData.json')
-      .then((currentLocation) => {
-        cy.intercept('GET','http://api.airvisual.com/v2/nearest_city?key=26e9573a-6960-4337-b548-ec068499ad9f', {
-          statusCode: 201,
-          body: currentLocation
-        })
-      })
     cy.visit('http://localhost:3000')
   })
 
@@ -15,3 +8,35 @@ describe('OzoneIQ', () => {
       // cy.url().should('include', '/')
   })
 })
+
+describe('OzoneIQ Dashboard Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+})
+
+  it('Should be able to click Current Location and navigate to dashboard with air quality information pertaining to that location', () => {
+      cy.get('form').children('.currentLocal').click()
+      cy.fixture('aqiData.json')
+      .then((currentLocation) => {
+        cy.intercept('GET','http://api.airvisual.com/v2/nearest_city?key=26e9573a-6960-4337-b548-ec068499ad9f', {
+          statusCode: 201,
+          body: currentLocation.current
+        })
+      })
+    // cy.url().should('include', '/')
+  })
+
+  it('Should be able to click Current Location and navigate to dashboard with air quality information pertaining to that location', () => {
+      cy.get('form').children('.currentLocal').click()
+      cy.fixture('aqiData.json')
+      .then((currentLocation) => {
+        cy.intercept('GET','http://api.airvisual.com/v2/nearest_city?key=26e9573a-6960-4337-b548-ec068499ad9f', {
+          statusCode: 201,
+          body: currentLocation.current
+        })
+      })
+    // cy.url().should('include', '/')
+  })
+})
+
+  
