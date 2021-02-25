a8c00e650768f1b25b459618ea5e0090f7270a2a
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-25 12:56:15 -0700

    Add test to stub network request and landing page

--- cypress/integration/ozoneiq.js
+++ cypress/integration/ozoneiq.js
@@ -1,11 +1,17 @@
 describe('OzoneIQ', () => {
   beforeEach(() => {
-    cy.fixture('movieData.json')
-      .then((data) => {
+    cy.fixture('aqiData.json')
+      .then((currentLocation) => {
         cy.intercept('GET','http://api.airvisual.com/v2/nearest_city?key=26e9573a-6960-4337-b548-ec068499ad9f', {
           statusCode: 201,
-          body: data
+          body: currentLocation
         })
       })
     cy.visit('http://localhost:3000')
   })
+
+  it('Should be able to visit the landing page with an option to select current location or choose a location', () => {
+      cy.get('form').children('.currentLocal', '.chooseLocal')
+      // cy.url().should('include', '/')
+  })
+})
