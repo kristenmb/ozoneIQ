3ba69f1c11c1c03623fa1655aef8daf6df1994ce
Kristen Bair <kristenmbair@gmail.com>
2021-02-25 15:40:11 -0700

    Move footer test to dashboard display, add testing outline for footer buttons

--- cypress/integration/ozoneiq.js
+++ cypress/integration/ozoneiq.js
@@ -1,9 +1,9 @@
 describe('OzoneIQ Landing Page', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('http://localhost:3000')
   })
 
-  it('Should be able to visit the landing page with logo, an option to select current location or choose a location', () => {
+  it ('Should be able to visit the landing page with logo, an option to select current location or choose a location', () => {
       cy
         .get('.logoWrapper').find('img').should('have.attr','src').should('include', '/static/media/logo4.1ade1652.png')
         .get('form').children('.currentLocal').should('be.visible')
@@ -11,17 +11,10 @@ describe('OzoneIQ Landing Page', () => {
       // cy.url().should('include', '/')
   })
 
-  it('Should have a footer with four possible icons to click', () => {
-      cy
-        .get('footer')
-        .children('img').should('have.length', 3)
-        .get('footer')
-        .children('p').contains('...')
-  })
 })
 
-describe('OzoneIQ Dashboard Page', () => {
-  beforeEach(() => {
+describe('OzoneIQ Dashboard Page - Current Location', () => {
+  before(() => {
       cy.fixture('aqiData.json')
       .then((data) => {
         cy.intercept('GET','http://api.airvisual.com/v2/nearest_city?key=26e9573a-6960-4337-b548-ec068499ad9f', {
@@ -32,9 +25,29 @@ describe('OzoneIQ Dashboard Page', () => {
     cy.visit('http://localhost:3000')
 })
 
-  it('Should be able to click Current Location and navigate to dashboard with air quality information pertaining to that location', () => {
+  it ('Should be able to click Current Location and navigate to dashboard with air quality information pertaining to that location', () => {
       cy.get('.currentLocal').click()
       cy.get('.main-dashboard').should('be.visible')
+  })
+
+  it ('Should have a footer with four possible icons to click', () => {
+      cy
+        .get('footer')
+        .children('.footerIcon').should('have.length', 3)
+        .get('footer')
+        .children('p').contains('...')
+  })
+  
+  it.skip ('Should be able to click the saved locations icon and be taken to the saved locations page', () => {
+
+  })
+
+  it.skip ('Should be able to click the question mark icon and be taken to the AQI information page', () => {
+    
+  })
+
+  it.skip ('Should be able to click the \'more\' icon and be taken to the about/contact page', () => {
+    
   })
 })
 
