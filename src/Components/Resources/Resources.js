e15603a02a9996449b0093c28f6dc18bdc9ec854
Kristen Bair <kristenmbair@gmail.com>
2021-02-24 21:51:45 -0700

    Create Resources component initial build

---
+++ src/Components/Resources/Resources.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Resources.css'
+
+const Resources = () => {
+  return (
+    <section className="resources-section">
+      <h2>How can I stay safe?</h2>
+      <p></p>
+      <h2>How can I limit my carbon footprint?</h2>
+      <p></p>
+    </section>
+  )
+}
+
+export default Resources
