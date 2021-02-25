e5dffecf2fd4469aea030fe67d81e3d9ebf9312d
Kristen Bair <kristenmbair@gmail.com>
2021-02-25 14:31:03 -0700

    Create aqiData display on Resources component using map with JSX render

--- src/Components/Resources/Resources.js
+++ src/Components/Resources/Resources.js
@@ -1,13 +1,32 @@
 import React from 'react'
+import { staySafeText, howToHelpText } from '../../aqiData'
 import './Resources.css'
 
 const Resources = () => {
+  const safetyHowTos = staySafeText.map(tip => {
+    return (
+      <li>{tip}</li>
+    )
+  })
+
+  const helpHowTos = howToHelpText.map(point => {
+    return (
+      <li>{point}</li>
+    )
+  })
+
   return (
     <section className="resources-section">
+      <article>
       <h2>How can I stay safe?</h2>
-      <p></p>
+      <ul>
+        {safetyHowTos}
+      </ul>
+      </article>
       <h2>How can I limit my carbon footprint?</h2>
-      <p></p>
+      <ul>
+        {helpHowTos}
+      </ul>
     </section>
   )
 }
