ebc6339dc2fe11c517513a7a9be38def4e5bc175
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-25 17:25:35 -0700

    Add links to buttons in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,16 +3,23 @@ import './Footer.css'
 import home from '../../assets/homebtn.png'
 import locations from '../../assets/locations.png'
 import questionmark from '../../assets/questionmark.png'
-
+import { Link } from 'react-router-dom'
 
 function Footer() {
-
   return (
     <footer>
-      <img className='footerIcon' src={home} alt='home button'/>
-      <img className='footerIcon' src={locations} alt='saved locations'/>
-      <img className='footerIcon' src={questionmark} alt='more info'/>
-      <p>...</p>
+      <Link to='/'>
+        <img className='footerIcon' src={home} alt='home button'/>
+      </Link>
+      <Link to='/saved-locations'>
+        <img className='footerIcon' src={locations} alt='saved locations'/>
+      </Link>
+      <Link to='/resources'>
+        <img className='footerIcon' src={questionmark} alt='more info'/>
+      </Link>
+      <Link to='/about-us'>
+        <p>...</p>
+      </Link>
     </footer>
   )
 }
