63e59e0ad027ab1dde15586682d10e028f1e80a4
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-25 21:07:47 -0700

    Add favicon, colored logo and edit manifest file

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './LandingPage.scss'
 import searchIcon from '../../assets/search.png'
 import locationIcon from '../../assets/location.svg'
-import logo from '../../assets/logo4.png'
+import logo from '../../assets/ozoneiq-color.png'
 import { Link } from 'react-router-dom'
 
 function LandingPage({grabUserLocationData, grabInputLocationData}) {
