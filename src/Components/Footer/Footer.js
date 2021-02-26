8296d2daf9e5c9d0a88dcf55e476321023dd47de
Kristen Bair <kristenmbair@gmail.com>
2021-02-25 20:17:10 -0700

    Add classnames to footer elements for easier targeting

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,10 +9,10 @@ function Footer() {
 
   return (
     <footer>
-      <img className='footerIcon' src={home} alt='home button'/>
-      <img className='footerIcon' src={locations} alt='saved locations'/>
-      <img className='footerIcon' src={questionmark} alt='more info'/>
-      <p>...</p>
+      <img className='footerIcon home-nav-btn' src={home} alt='home button'/>
+      <img className='footerIcon saved-local-nav-btn' src={locations} alt='saved locations'/>
+      <img className='footerIcon aqi-nav-btn' src={questionmark} alt='more info'/>
+      <p className='more-nav-btn'>...</p>
     </footer>
   )
 }
