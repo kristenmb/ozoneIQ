70d00b691dd60040250789752378fd15b29d9ae4
Kristen Bair <kristenmbair@gmail.com>
2021-02-28 16:28:21 -0700

    Enable offline mode for pwa with service worker caching

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,45 @@
+const pwaCache = 'pwa-cache-1'
+const staticCache = [
+      '/index.html',
+      '/index.css',
+      '/index.js',
+      '/logo4.png',
+      '/utilities.js',
+      '/RB.png',
+      '/LM.png',
+      '/KB.png',
+      '/location.svg'
+    ]
+
 self.addEventListener('install', (e) => {
-  
-  console.log('SW install event')
+  e.waitUntil(
+    caches.open(pwaCache)
+      .then(cache => cache.addAll(staticCache))
+  )
 })
 
 self.addEventListener('activate', (e) => {
-  console.log('SW activate event')
+  let cacheCleaned = caches.keys()
+    .then(keys => {
+      keys.forEach(key => {
+        if (key !== pwaCache) {
+          return caches.delete(key)
+        }
+      })
+    })
+  e.waitUntil(cacheCleaned)
 })
+
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) return res
+
+      return fetch(e.request).then((newRes) => {
+        caches.open(pwaCache).then((cache) => cache.put(e.request, newRes))
+        return newRes.clone()
+      })
+    })
+  )
+  })
--- src/index.js
+++ src/index.js
@@ -26,15 +26,20 @@ if (navigator.serviceWorker) {
 serviceWorkerRegistration.register();
 
 
-if (window.caches) {
-  caches.open('pwa-v1.1').then((cache) => {
-    cache.addAll([
-      '/index.html',
-      '/index.css',
-      '/index.js'
-    ])
-  })
-}
+// if (window.caches) {
+//   caches.open('pwa-v1.1').then((cache) => {
+//     cache.addAll([
+//       '/index.html',
+//       '/index.css',
+//       '/index.js',
+//       'logo4.png',
+//       'RB.png',
+//       'LM.png',
+//       'KB.png',
+//       'location.svg'
+//     ])
+//   })
+// } 
 
 
 // If you want to start measuring performance in your app, pass a function
