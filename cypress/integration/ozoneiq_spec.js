d65eac9a06115111ee0497d3d07ab78a60abffae
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-28 17:35:58 -0700

    Add testing

--- cypress/integration/ozoneiq_spec.js
+++ cypress/integration/ozoneiq_spec.js
@@ -8,7 +8,7 @@ describe('OzoneIQ Landing Page', () => {
         .get('.logoWrapper').find('img').should('have.attr','src').should('include', '/static/media/logo4.1ade1652.png')
         .get('form').find('.currentLocal').should('be.visible')
         .get('form').find('.chooseLocal').should('be.visible')
-      // cy.url().should('include', 'dashboard')
+      cy.url().should('include', '/')
   })
 
 })
@@ -26,33 +26,40 @@ describe('OzoneIQ Dashboard Page - Current Location', () => {
 })
 
   it ('Should be able to click Current Location and navigate to dashboard with air quality information pertaining to that location', () => {
-      cy.get('.currentLocal').click()
+      cy
+        .get('.currentLocal').click()
         .get('.main-dashboard').should('be.visible')
       cy.url().should('include', 'dashboard')
   })
 
   it ('Should display the current city and temperature', () => {
-    cy.get('.location-name-temp-container').find('.location-name').should('contain', 'Centennial, Colorado')
+    cy
+      .get('.location-name-temp-container').find('.location-name').should('contain', 'Centennial, Colorado')
       .get('.temp-container').find('.temperature').should('contain', '30 F')
   })
 
   it ('Should display the current AQI', () => {
-    cy.get('.aqi-container').find('.aqi-level').should('contain', 'GOOD')
+    cy
+      .get('.aqi-container').find('.aqi-level').should('contain', 'GOOD')
       .get('.aqi-container').find('.aqi-number').should('contain', '27')
       .get('.aqi-container').find('.aqi-description').should('contain', 'satisfactory')
 
   })
 
   it ('Should display the current main pollutant, humidity, pressure, and wind speed', () => {
-    cy.get('.additional-info-container').get('.additional-info').find('.pollutant-icon').should('have.attr', 'alt', 'Outline of smoke stack')
+    cy
+      .get('.additional-info-container').get('.additional-info').find('.pollutant-icon').should('have.attr', 'alt', 'Outline of smoke stack')
       .get('.additional-info-container').get('.additional-info').find('.pollutant').should('contain', 'o3')
-  
+    
+    cy
       .get('.additional-info-container').get('.additional-info').find('.humidity-icon').should('have.attr', 'alt', 'Outline of rain drop')
       .get('.additional-info-container').get('.additional-info').find('.humidity').should('contain', 69)
-
+    
+    cy
       .get('.additional-info-container').get('.additional-info').find('.pressure-icon').should('have.attr', 'alt', 'Outline of a pressure guage')
       .get('.additional-info-container').get('.additional-info').find('.pressure').should('contain', '1020')
-
+   
+    cy
       .get('.additional-info-container').get('.additional-info').find('.wind-icon').should('have.attr', 'alt', 'Outline of wind blowing')
       .get('.additional-info-container').get('.additional-info').find('.wind').should('contain', '7 mph N')
   })
@@ -100,35 +107,41 @@ describe('OzoneIQ Dashboard Page - Chosen Location', () => {
 })
 
   it ('Should be able to input a Chosen Location and navigate to dashboard with air quality information pertaining to that location', () => {
-      cy.get('form input').type('lafayette, louisiana, usa')
+      cy
+        .get('form input').type('lafayette, louisiana, usa')
         .get('.chooseLocal .searchButton').click()
         .get('.main-dashboard').should('be.visible')
   })
 
    it ('Should display the current city and temperature', () => {
-      cy.get('.location-name-temp-container').find('.location-name').should('contain', 'Lafayette, Louisiana')
+      cy
+        .get('.location-name-temp-container').find('.location-name').should('contain', 'Lafayette, Louisiana')
         .get('.temp-container').find('.temperature').should('contain', '73 F')
   })
 
    it ('Should display the current AQI', () => {
-    cy.get('.aqi-container').find('.aqi-level').should('contain', 'GOOD')
+    cy
+      .get('.aqi-container').find('.aqi-level').should('contain', 'GOOD')
       .get('.aqi-container').find('.aqi-number').should('contain', '4')
       .get('.aqi-container').find('.aqi-description').should('contain', 'satisfactory')
-
   })
 
   it ('Should display the current main pollutant, humidity, pressure, and wind speed', () => {
-    cy.get('.additional-info-container').get('.additional-info').find('.pollutant-icon').should('have.attr', 'alt', 'Outline of smoke stack')
-    cy.get('.additional-info-container').get('.additional-info').find('.pollutant').should('contain', 'p2')
+    cy
+      .get('.additional-info-container').get('.additional-info').find('.pollutant-icon').should('have.attr', 'alt', 'Outline of smoke stack')
+      .get('.additional-info-container').get('.additional-info').find('.pollutant').should('contain', 'p2')
   
-    cy.get('.additional-info-container').get('.additional-info').find('.humidity-icon').should('have.attr', 'alt', 'Outline of rain drop')
-    cy.get('.additional-info-container').get('.additional-info').find('.humidity').should('contain', 64)
+    cy
+      .get('.additional-info-container').get('.additional-info').find('.humidity-icon').should('have.attr', 'alt', 'Outline of rain drop')
+      .get('.additional-info-container').get('.additional-info').find('.humidity').should('contain', 64)
 
-    cy.get('.additional-info-container').get('.additional-info').find('.pressure-icon').should('have.attr', 'alt', 'Outline of a pressure guage')
-    cy.get('.additional-info-container').get('.additional-info').find('.pressure').should('contain', '1019')
+    cy
+      .get('.additional-info-container').get('.additional-info').find('.pressure-icon').should('have.attr', 'alt', 'Outline of a pressure guage')
+      .get('.additional-info-container').get('.additional-info').find('.pressure').should('contain', '1019')
 
-    cy.get('.additional-info-container').get('.additional-info').find('.wind-icon').should('have.attr', 'alt', 'Outline of wind blowing')
-    cy.get('.additional-info-container').get('.additional-info').find('.wind').should('contain', '4 mph S')
+    cy
+      .get('.additional-info-container').get('.additional-info').find('.wind-icon').should('have.attr', 'alt', 'Outline of wind blowing')
+      .get('.additional-info-container').get('.additional-info').find('.wind').should('contain', '4 mph S')
   })
 
   it ('Should have a footer with four possible icons to click', () => {
@@ -143,15 +156,22 @@ describe('OzoneIQ Dashboard Page - Chosen Location - Error Handling 404 status',
   before(() => {
       cy.intercept('GET','http://api.airvisual.com/v2/nearest_city?key=26e9573a-6960-4337-b548-ec068499ad9f', {
         statusCode: 404
-          })
-    })
+      })
+      cy.visit('http://localhost:3000')
+  })
 
-  it.skip ('Should display user friend error handling about how to find a location included in api data', () => {
+  it ('Should display user friend error handling about how to find a location included in api data', () => {
 
   })
 })
 
 describe('OzoneIQ AQI Info Page', () => {
+   before(() => {
+      cy.intercept('GET','http://api.airvisual.com/v2/nearest_city?key=26e9573a-6960-4337-b548-ec068499ad9f', {
+        statusCode: 404
+      })
+      cy.visit('http://localhost:3000')
+  })
   
   it.skip ('Should be able to navigate to the AQI Info page', () => {
     cy.get('footer').find('.aqi-nav-btn').click()
@@ -169,13 +189,20 @@ describe('OzoneIQ AQI Info Page', () => {
   })
 })
 
-describe('OzoneIQ Resources Page', () => {
+
+describe.only('OzoneIQ Resources Page', () => {
+  before(() => {
+    cy.intercept('GET','http://api.airvisual.com/v2/nearest_city?key=26e9573a-6960-4337-b548-ec068499ad9f', {
+      statusCode: 404
+    })
+    cy.visit('http://localhost:3000')
+  })
   
-  it.skip ('Should be able to navigate to the Resources page', () => {
+  it ('Should be able to navigate to the Resources page', () => {
    
   })
 
-  it.skip ('Should display the resources information', () => {
+  it ('Should display the resources information', () => {
 
   })
 
