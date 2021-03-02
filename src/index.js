db0b6f6c4a9eefd73d2e9ad2fe9902c5bba36244
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-02 11:53:39 -0700

    Lint index.js

--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ import './index.css';
 import App from './Components/App/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('../../sw.js')
-    .then((registration) => {
-      console.log('SW Registered')
-    })
+    navigator.serviceWorker.register('../../sw.js')
+        .then((registration) => {
+            console.log('SW Registered', registration);
+        });
 }
 
 serviceWorkerRegistration.register();
