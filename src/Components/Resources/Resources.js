a7fa637e9bc6b143641b83e3e65f5a572fcbde08
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-02 12:19:00 -0700

    Lint resources file

--- src/Components/Resources/Resources.js
+++ src/Components/Resources/Resources.js
@@ -1,43 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { staySafeText, howToHelpText } from '../../aqiData'
-import leftArrow from '../../assets/left-arrow.png'
-import './Resources.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { staySafeText, howToHelpText } from '../../aqiData';
+import leftArrow from '../../assets/left-arrow.png';
+import './Resources.scss';
 
 const Resources = () => {
-  const safetyHowTos = staySafeText.map((tip, i) => {
-    return (
-      <li key={i}>{tip}</li>
-    )
-  })
+    const safetyHowTos = staySafeText.map((tip, i) => {
+        return (
+            <li key={i}>{tip}</li>
+        );
+    });
 
-  const helpHowTos = howToHelpText.map((point, i) => {
-    return (
-      <li key={i}>{point}</li>
-    )
-  })
+    const helpHowTos = howToHelpText.map((point, i) => {
+        return (
+            <li key={i}>{point}</li>
+        );
+    });
 
-  return (
-    <section className="resources-section">
-      <article className="resources-info">
-        <h2 className="resources-qs">How can I stay safe?</h2>
-        <ul className="text-area">
-          {safetyHowTos}
-        </ul>
-      </article>
-      <article className="resources-info">
-        <h2 className="resources-qs">How can I limit my carbon footprint?</h2>
-        <ul className="text-area">
-          {helpHowTos}
-        </ul>
-      </article>
-      <Link to='/resources'>
-        <button className='back-btn'>
-          <img src={leftArrow} alt='Thin arrow pointing left'/>
-          Back</button>
-      </Link>
-    </section>
-  )
+    return (
+        <section className="resources-section">
+            <article className="resources-info">
+                <h2 className="resources-qs">How can I stay safe?</h2>
+                <ul className="text-area">
+                    {safetyHowTos}
+                </ul>
+            </article>
+            <article className="resources-info">
+                <h2 className="resources-qs">How can I limit my carbon footprint?</h2>
+                <ul className="text-area">
+                    {helpHowTos}
+                </ul>
+            </article>
+            <Link to='/resources'>
+                <button className='back-btn'>
+                <img src={leftArrow} alt='Thin arrow pointing left'/>
+                 Back</button>
+            </Link>
+        </section>
+    );
 }
 
-export default Resources
+export default Resources;
