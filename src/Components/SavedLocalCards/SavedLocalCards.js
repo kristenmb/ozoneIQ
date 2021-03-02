55d2e6071079f142980723cdf9e7ff94e769688b
Rachel Buchta <rachelbuchta@yahoo.com>
2021-03-02 12:12:24 -0700

    Lint saved local cards

--- src/Components/SavedLocalCards/SavedLocalCards.js
+++ src/Components/SavedLocalCards/SavedLocalCards.js
@@ -1,25 +1,25 @@
-import './SavedLocalCards.scss'
-import { Link } from 'react-router-dom'
+import './SavedLocalCards.scss';
+import { Link } from 'react-router-dom';
 import x from '../../assets/remove.png';
+import React from 'react';
 
 const SavedLocalCards = ({location, removeFromFavorites, id, grabInputLocationData}) => {
+    return (
+        <section className="saved-local-section">
+            <img 
+                className='delete-icon icon' 
+                onClick={removeFromFavorites} 
+                src={x} 
+                id={id}
+                alt='favorited star'/>
+            <Link 
+                className='linkWrapper' 
+                onClick={(event)=> grabInputLocationData(location.city, location.state, location.country, event)} 
+                to='/dashboard'>
+                <h1 className='location'>{location.city}, {location.state}, {location.country}</h1>
+            </Link>
+        </section>
+    );
+};
 
-  return (
-    <section className="saved-local-section">
-          <img 
-            className='delete-icon icon' 
-            onClick={removeFromFavorites} 
-            src={x} 
-            id={id}
-            alt='favorited star'/>
-        <Link 
-          className='linkWrapper' 
-          onClick={(event)=> grabInputLocationData(location.city, location.state, location.country, event)} 
-          to='/dashboard'>
-            <h1 className='location'>{location.city}, {location.state}, {location.country}</h1>
-        </Link>
-    </section>
-  )
-}
-
-export default SavedLocalCards
+export default SavedLocalCards;
